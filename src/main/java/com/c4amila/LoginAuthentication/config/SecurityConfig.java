7c7591d3e71817c79c8e519a6baa7ee71ebc58f1
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-07 14:01:49 -0300

    refactor: ajuste nas configurações do Spring Security

--- src/main/java/com/c4amila/LoginAuthentication/config/SecurityConfig.java
+++ src/main/java/com/c4amila/LoginAuthentication/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.c4amila.LoginAuthentication.config;
 
+import com.c4amila.LoginAuthentication.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -21,7 +30,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()) //desabilita csrf
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //libera as rotas
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/usuarios/cadastro",
+                                "/usuarios/login",
+                                "/usuarios/recuperar-senha",
+                                "/usuarios/confirmar-senha"
+                        ).permitAll().anyRequest().authenticated())
+                        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); //libera as rotas
 
         return http.build();
     }
