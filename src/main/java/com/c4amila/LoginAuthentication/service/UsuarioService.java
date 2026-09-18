111f26d262ec3fc355b51c490ed8ba1b0d494f46
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-18 00:37:49 -0300

    refactor(service): refatora toda regra de negócio em UsuarioService

--- src/main/java/com/c4amila/LoginAuthentication/service/UsuarioService.java
+++ src/main/java/com/c4amila/LoginAuthentication/service/UsuarioService.java
@@ -5,12 +5,14 @@ import com.c4amila.LoginAuthentication.exception.*;
 import com.c4amila.LoginAuthentication.model.Usuario;
 import com.c4amila.LoginAuthentication.repository.UsuarioRepository;
 import com.c4amila.LoginAuthentication.security.TokenService;
+import org.springframework.cglib.core.Local;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.security.SecureRandom;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -22,6 +24,10 @@ public class UsuarioService {
     private static final SecureRandom secureRandom = new SecureRandom();
 
     private static final int LIMITE_TENTATIVAS = 5;
+    private static final int LIMITE_TENTATIVAS_RECUPERACAO = 5;
+    private static final int MINUTOS_BLOQUEIO = 5;
+    private static final int MINUTOS_BLOQUEIO_RECUPERACAO = 5;
+    private static final int MIN_EXPIRACAO_CODIGO = 5;
 
     public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, EmailService emailService, TokenService tokenService) {
         this.usuarioRepository = usuarioRepository;
@@ -31,7 +37,7 @@ public class UsuarioService {
     }
 
     public UsuarioResponseDTO cadastrar(UsuarioCadastroRequestDTO dto){
-        boolean emailExiste = usuarioRepository.findByEmail(dto.getEmail()).isPresent();
+        boolean emailExiste = usuarioRepository.existsByEmail(dto.getEmail());
         if (emailExiste){
             throw new EmailCadastradoException("Este e-mail já está cadastrado no sistema");
         }
@@ -41,99 +47,51 @@ public class UsuarioService {
         novoUsuario.setDataNascimento(dto.getDataNascimento());
         novoUsuario.setEmail(dto.getEmail());
         novoUsuario.setTelefone(dto.getTelefone());
-        novoUsuario.setSenha(passwordEncoder.encode(dto.getSenha())); //agora com encoder
-
-        //inicializa os campos de controle de senha
-        novoUsuario.setTentativaSenha(0);
-        novoUsuario.setEstaBloqueado(false);
-        novoUsuario.setHorarioBloqueio(null);
+        novoUsuario.setSenha(passwordEncoder.encode(dto.getSenha()));
 
         Usuario usuarioSalvo = usuarioRepository.save(novoUsuario);
 
-        UsuarioResponseDTO response = new UsuarioResponseDTO();
-        response.setId(usuarioSalvo.getId());
-        response.setNomeCompleto(usuarioSalvo.getNomeCompleto());
-        response.setDataNascimento(usuarioSalvo.getDataNascimento());
-        response.setEmail(usuarioSalvo.getEmail());
-        response.setTelefone(usuarioSalvo.getTelefone());
-
-        return response;
+        return criarUsuarioResponseDTO(usuarioSalvo);
     }
 
     public LoginResponseDTO autenticar(UsuarioLoginRequestDTO dto){
         Usuario usuario = usuarioRepository.findByEmail(dto.getEmail())
                 .orElseThrow(() -> new CredenciaisInvalidasException("E-mail ou senha inválido"));
 
-        if (usuario.getEstaBloqueado()){
-            if (usuario.getHorarioBloqueio() != null){
-                LocalDateTime now = LocalDateTime.now();
-                LocalDateTime horarioDesbloqueio = usuario.getHorarioBloqueio().plusMinutes(5); //add os 5 min
+        LocalDateTime now = LocalDateTime.now();
 
-                if (now.isAfter(horarioDesbloqueio)){
-                    usuario.setEstaBloqueado(false);
-                    usuario.setTentativaSenha(0);
-                    usuario.setHorarioBloqueio(null);
-
-                    usuarioRepository.save(usuario);
-                }else{
-                    long minRestantes = ChronoUnit.MINUTES.between(now, horarioDesbloqueio);
-                    throw new ContaBloqueadaException("Sua conta está temporariamente bloqueada. Tente novamente em "
-                            + (minRestantes + 1) + " minutos.");
-                }
-            }else {
-                usuario.setEstaBloqueado(false);
-                usuario.setTentativaSenha(0);
-            }
-        }
+        verificarBloqueioLogin(usuario, now);
 
         boolean senhaCorreta = passwordEncoder.matches(dto.getSenha(), usuario.getSenha());
-        if (senhaCorreta){
-            usuario.setTentativaSenha(0);
-            usuarioRepository.save(usuario);
 
-            String token = tokenService.gerarToken(usuario.getEmail());
-
-            UsuarioResponseDTO usuarioResponseDTO = new UsuarioResponseDTO(
-                    usuario.getId(),
-                    usuario.getNomeCompleto(),
-                    usuario.getDataNascimento(),
-                    usuario.getEmail(),
-                    usuario.getTelefone()
-            );
-
-            return new LoginResponseDTO(token, usuarioResponseDTO);
-
-        }else{
-            int incrementaTentativa = usuario.getTentativaSenha() + 1;
-            usuario.setTentativaSenha(incrementaTentativa);
-
-            if (incrementaTentativa >= LIMITE_TENTATIVAS){//se sim, bloqueia por 5min
-                usuario.setEstaBloqueado(true);
-                usuario.setHorarioBloqueio(LocalDateTime.now());
-
-                usuarioRepository.save(usuario);
-
-                throw new ContaBloqueadaException("5 tentativas incorretas. Você está bloqueado por 5 minutos");
-            }
-
-            //avisar o usuario conforme o saldo de tentativas diminui
-            usuarioRepository.save(usuario);
-            int tentativasRestantes = 5 - incrementaTentativa;
-            throw new CredenciaisInvalidasException("E-mail ou senha inválidos. Você tem mais " + tentativasRestantes + " tentativa(s)");
+        if (!senhaCorreta){
+            registrarTentativaLoginInvalida(usuario, now);
         }
+        resetarTentativasLogin(usuario);
+
+        String token = tokenService.gerarToken(usuario.getEmail());
+
+        UsuarioResponseDTO usuarioResponseDTO = criarUsuarioResponseDTO(usuario);
+
+        return new LoginResponseDTO(token, usuarioResponseDTO);
     }
 
     public void solicitarRecuperacaoSenha(RecuperacaoSolicitacaoDTO dto){
-        Usuario usuario = usuarioRepository.findByEmail(dto.getEmail()).orElseThrow(
-                () -> new RequisicaoInvalidaException("E-mail não encontrado"));
+        Optional<Usuario> usuarioOpt = usuarioRepository.findByEmail(dto.getEmail());
 
-        int geracaoNum = secureRandom.nextInt(1_000_000);
-        String codigo = String.format("%06d", geracaoNum); //gera codigo aleatorio
+        if (usuarioOpt.isEmpty()){
+            return;
+        }
 
-        //salva o codigo e o horario atual da geracao
-        usuario.setCodigoRecuperacao(codigo);
-        usuario.setHorarioGeracaoCodigo(LocalDateTime.now());
-        usuarioRepository.save(usuario); //salva tudo
+        Usuario usuario = usuarioOpt.get();
+
+        String codigo = gerarCodigoRecuperacao();
+        String codigoHash = passwordEncoder.encode(codigo);
+
+        usuario.setCodigoRecuperacao(codigoHash);
+        usuario.setHorarioExpiracaoCodigo(LocalDateTime.now().plusMinutes(MIN_EXPIRACAO_CODIGO));
+
+        usuarioRepository.save(usuario);
 
         emailService.enviarEmailRecuperacao(usuario.getEmail(), usuario.getNomeCompleto(), codigo);
     }
@@ -144,49 +102,26 @@ public class UsuarioService {
         }
 
         Usuario usuario = usuarioRepository.findByEmail(dto.getEmail()).orElseThrow(
-                () -> new RequisicaoInvalidaException("E-mail não encontrado no sistema")
+                () -> new RequisicaoInvalidaException("Dados de recuperação inválidos")
         );
 
-        if (usuario.getHorarioBloqueio() != null){
-            if (usuario.getHorarioBloqueio().isAfter(LocalDateTime.now())){
-                throw new ContaBloqueadaException("Conta bloqueada temporariamente. Tente novamente mais tarde");
-            }
-            usuario.setHorarioBloqueio(null);
-            usuario.setTentativaSenha(0);
-            usuario.setEstaBloqueado(false);
+        LocalDateTime now = LocalDateTime.now();
+        verificarBloqueioRecuperacaoSenha(usuario, now);
+
+        if(usuario.getCodigoRecuperacao() == null ||
+                usuario.getCodRecuperacaoExpiraEm() == null ||
+                now.isAfter(usuario.getCodRecuperacaoExpiraEm())){
+
+            throw new RequisicaoInvalidaException("Código de recuperação inválido ou expirado. Solicite um novo código");
         }
 
-        if(usuario.getHorarioGeracaoCodigo() == null ||
-            usuario.getHorarioGeracaoCodigo().isBefore(LocalDateTime.now().minusMinutes(5))){
-            throw new RequisicaoInvalidaException("O código expirou. Solicite um novo código");
-        }
-
-        //compara o codigo enviado com o do banco
-        if (usuario.getCodigoRecuperacao() == null || !usuario.getCodigoRecuperacao().equals(dto.getCodigo())){
-            int tentativas = usuario.getTentativaSenha() + 1;
-            usuario.setTentativaSenha(tentativas);
-            int limite = 5;
-
-            if (tentativas >= limite){
-                usuario.setHorarioBloqueio(LocalDateTime.now().plusMinutes(5));
-                usuario.setEstaBloqueado(true);
-                usuarioRepository.save(usuario);
-
-                throw new ContaBloqueadaException("Número de tentativas excedido. Conta bloqueada por 5 minutos");
-            }
-
-            usuarioRepository.save(usuario);
-            int tentativasRestantes = limite - tentativas;
-            throw new CredenciaisInvalidasException("Código de verificação inválido. Você tem mais " + tentativasRestantes + " tentativas");
+        boolean codigoCorreto = passwordEncoder.matches(dto.getCodigo(), usuario.getCodigoRecuperacao());
+        if (!codigoCorreto){
+            registrarTentativaRecuperacaoInvalida(usuario, now);
         }
 
         //atualização da senha
-        usuario.setSenha(passwordEncoder.encode(dto.getNovaSenha()));
-        usuario.setCodigoRecuperacao(null);
-        usuario.setHorarioGeracaoCodigo(null);
-        usuario.setTentativaSenha(0); //se a conta estava bloqueada, aproveita para resetar as tentativas
-        usuario.setHorarioBloqueio(null);
-        usuario.setEstaBloqueado(false);
+        atualizarSenha(usuario, dto.getNovaSenha());
 
         usuarioRepository.save(usuario);
     }
@@ -194,6 +129,119 @@ public class UsuarioService {
     public void logout(LogoutDTO dto){
         Usuario usuario = usuarioRepository.findByEmail(dto.getEmail())
                 .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado"));
+
+        // TODO: após implementar refresh token, adicionar revogação sessao/token
+    }
+
+
+    //--------------------
+    private void verificarBloqueioLogin(Usuario usuario, LocalDateTime now){
+        if (usuario.getLoginBloqueadoAte() == null){
+            return;
+        }
+
+        if (now.isBefore(usuario.getLoginBloqueadoAte())){
+            long minRestante = ChronoUnit.MINUTES.between(now, usuario.getLoginBloqueadoAte());
+
+            throw new ContaBloqueadaException(
+                    "Sua conta está temporariamente bloqueada. Tente novamente em "
+                    + (minRestante + 1) + " minuto(s)"
+            );
+        }
+
+        usuario.setTentativaLogin(0);
+        usuario.setLoginBloqueadoAte(null);
+
+        usuarioRepository.save(usuario);
+
+    }
+    private void verificarBloqueioRecuperacaoSenha(Usuario usuario, LocalDateTime now){
+        if (usuario.getCodRecuperacaoBloqueadoAte() == null){
+            return;
+        }
+
+        if (now.isBefore(usuario.getCodRecuperacaoBloqueadoAte())){
+            throw new ContaBloqueadaException(
+                    "Número de tentativas excedido. Sua conta está temporariamente bloqueada"
+            );
+        }
+
+        usuario.setCodRecuperacaoBloqueadoAte(null);
+        usuario.setTentativasRecuperacao(0);
+
+        usuarioRepository.save(usuario);
+    }
+    private void registrarTentativaLoginInvalida(Usuario usuario, LocalDateTime now){
+        int tentativasLogin = usuario.getTentativaLogin() + 1;
+        usuario.setTentativaLogin(tentativasLogin);
+
+        if (tentativasLogin >= LIMITE_TENTATIVAS){
+            usuario.setLoginBloqueadoAte(now.plusMinutes(MINUTOS_BLOQUEIO));
+            usuario.setTentativaLogin(0);
+            usuarioRepository.save(usuario);
+
+            throw new ContaBloqueadaException(
+                    LIMITE_TENTATIVAS + " tentativas incorretas. " +
+                            "Você está bloqueado por " +
+                            MINUTOS_BLOQUEIO + "5 minutos"
+            );
+        }
+
+        usuarioRepository.save(usuario);
+        int tentativasRestantes = LIMITE_TENTATIVAS - usuario.getTentativaLogin();
+
+        throw new CredenciaisInvalidasException(
+                "E-mail ou senha inválidos. Você tem mais " + tentativasRestantes + " tentativa(s)"
+        );
+    }
+    private void registrarTentativaRecuperacaoInvalida(Usuario usuario, LocalDateTime now){
+        int tentativas = usuario.getTentativasRecuperacao() + 1;
+        usuario.setTentativasRecuperacao(tentativas);
+
+        if (tentativas >= LIMITE_TENTATIVAS_RECUPERACAO){
+            usuario.setCodRecuperacaoBloqueadoAte(now.plusMinutes(MINUTOS_BLOQUEIO_RECUPERACAO));
+
+            usuario.setTentativasRecuperacao(0);
+            usuarioRepository.save(usuario);
+
+            throw new ContaBloqueadaException("Número de tentativas excedido. Conta bloqueada temporariamente");
+        }
+
+        usuarioRepository.save(usuario);
+
+        int tentativasRestantes = LIMITE_TENTATIVAS_RECUPERACAO - tentativas;
+        throw new CredenciaisInvalidasException("Código de verificação inválido. Você tem mais " + tentativasRestantes + " tentativas");
+
+    }
+    private void resetarTentativasLogin(Usuario usuario){
+        usuario.setTentativaLogin(0);
+        usuario.setLoginBloqueadoAte(null);
+
+        usuarioRepository.save(usuario);
+    }
+    private UsuarioResponseDTO criarUsuarioResponseDTO(Usuario usuario){
+        return new UsuarioResponseDTO(
+                usuario.getId(),
+                usuario.getNomeCompleto(),
+                usuario.getDataNascimento(),
+                usuario.getEmail(),
+                usuario.getTelefone()
+        );
+    }
+    private String gerarCodigoRecuperacao(){
+        int geracaoNum = secureRandom.nextInt(1_000_000);
+
+        return String.format("%06d", geracaoNum);
+    }
+    private void atualizarSenha(Usuario usuario, String senha){
+        usuario.setSenha(passwordEncoder.encode(senha));
+
+        usuario.setCodigoRecuperacao(null);
+        usuario.setCodRecuperacaoExpiraEm(null);
+        usuario.setTentativasRecuperacao(0);
+        usuario.setCodRecuperacaoBloqueadoAte(null);
+
+        usuarioRepository.save(usuario);
     }
 
 }
