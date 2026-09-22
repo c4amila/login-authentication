1b1888151edfe936598f4ba534c25f5394f47a83
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-22 00:35:05 -0300

    feat(security): adiciona rota para verificar-conta em SecurityConfig

--- src/main/java/com/c4amila/LoginAuthentication/config/SecurityConfig.java
+++ src/main/java/com/c4amila/LoginAuthentication/config/SecurityConfig.java
@@ -18,10 +18,10 @@ public class SecurityConfig {
 
     private static final String[] ROTAS_PUBLICAS =  {
             "/usuarios/cadastro",
+            "/usuarios/verificar-conta",
             "/usuarios/login",
             "/usuarios/recuperar-senha",
             "/usuarios/confirmar-senha",
-            "/usuarios/sair",
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html"
