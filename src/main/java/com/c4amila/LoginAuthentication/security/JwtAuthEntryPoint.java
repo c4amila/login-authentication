90f787430008cb26791e3c989627535c16676579
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-22 01:12:02 -0300

    fix(security): ajusta parametro para AuthenticationException

--- src/main/java/com/c4amila/LoginAuthentication/security/JwtAuthEntryPoint.java
+++ src/main/java/com/c4amila/LoginAuthentication/security/JwtAuthEntryPoint.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
@@ -25,7 +26,7 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
-            AuthenticationEntryPoint authException
+            AuthenticationException authException
     ) throws IOException, ServletException {
         ErroResponseDTO erro = new ErroResponseDTO(
                 LocalDateTime.now(),
